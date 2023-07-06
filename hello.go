3fed38fa742b9ad4816683ca47afcfbc1bbf980f
Tilak Patel <ptilak142@gmail.com>
2023-07-05 23:16:50 -0400

    verbose output

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/PagerDuty/go-pagerduty"
 	"os"
 )
@@ -11,10 +12,19 @@ var userId = os.Getenv("PD_USER_ID")
 var email = os.Getenv("PD_EMAIL")
 
 func main() {
+	if authToken == "" || userId == "" || email == "" {
+		fmt.Println("Missing env variables!!!")
+		os.Exit(1)
+	}
 	client := pagerduty.NewClient(authToken)
 	eps, err := client.ListIncidentsWithContext(context.TODO(), pagerduty.ListIncidentsOptions{Limit: 500, Statuses: []string{"triggered"}, UserIDs: []string{userId}})
 	if err != nil {
 		panic(err)
+	}
+	if len(eps.Incidents) == 0 {
+		fmt.Println("No alerts detected.")
+	} else {
+		fmt.Sprintf("%d incidents detected", len(eps.Incidents))
 	}
 	for _, p := range eps.Incidents {
 		// ack all of the incidents
