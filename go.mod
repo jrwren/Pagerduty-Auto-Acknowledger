6a6d9476069f0b06346c4b5763c7265229c289a9
Tilak Patel <ptilak142@gmail.com>
2023-07-24 20:46:52 -0400

    run on schedule

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require github.com/PagerDuty/go-pagerduty v1.7.0
 
-require github.com/google/go-querystring v1.1.0 // indirect
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/robfig/cron/v3 v3.0.0 // indirect
+)
--- hello.go
+++ hello.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 	"github.com/PagerDuty/go-pagerduty"
 	"os"
+	"github.com/robfig/cron/v3"
+	"os/signal"
+	"syscall"
 )
 
 var authToken = os.Getenv("PD_AUTH_TOKEN")
 var userId = os.Getenv("PD_USER_ID")
 var email = os.Getenv("PD_EMAIL")
 
-func main() {
+func checkAndAcknowledgeAlert() {
 	if authToken == "" || userId == "" || email == "" {
 		fmt.Println("Missing env variables!!!")
 		os.Exit(1)
@@ -31,4 +34,15 @@ func main() {
 		id := p.ID
 		client.ManageIncidentsWithContext(context.TODO(), email, []pagerduty.ManageIncidentsOptions{{ID: id, Status: "acknowledged"}})
 	}
+
+}
+
+func main() {
+    c := cron.New()
+    c.AddFunc("@every 5s", checkAndAcknowledgeAlert)
+    c.Start()
+    done := make(chan os.Signal, 1)
+    signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+    <-done
+    c.Stop()
 }
