3b7c02a55623dd64a125edbf774997ac288dcd37
Jay Wren <jawren@cisco.com>
2023-10-10 10:40:31 -0400

    make it mine

--- hello.go
+++ hello.go
@@ -1,48 +1,93 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
-	"github.com/PagerDuty/go-pagerduty"
 	"os"
-	"github.com/robfig/cron/v3"
 	"os/signal"
+	"strings"
 	"syscall"
+	"time"
+
+	"github.com/PagerDuty/go-pagerduty"
 )
 
 var authToken = os.Getenv("PD_AUTH_TOKEN")
 var userId = os.Getenv("PD_USER_ID")
 var email = os.Getenv("PD_EMAIL")
+var client *pagerduty.Client
+var noack []string
+var timer *time.Timer
+var freq = 5 * time.Second
 
 func checkAndAcknowledgeAlert() {
-	if authToken == "" || userId == "" || email == "" {
-		fmt.Println("Missing env variables!!!")
-		os.Exit(1)
-	}
-	client := pagerduty.NewClient(authToken)
-	eps, err := client.ListIncidentsWithContext(context.TODO(), pagerduty.ListIncidentsOptions{Limit: 500, Statuses: []string{"triggered"}, UserIDs: []string{userId}})
+	defer func() {
+		timer = time.AfterFunc(freq, checkAndAcknowledgeAlert)
+	}()
+	eps, err := client.ListIncidentsWithContext(context.TODO(),
+		pagerduty.ListIncidentsOptions{
+			Limit:    500,
+			Statuses: []string{"triggered"},
+			UserIDs:  []string{userId}},
+	)
 	if err != nil {
-		panic(err)
-	}
-	if len(eps.Incidents) == 0 {
-		fmt.Println("No alerts detected.")
-	} else {
-		fmt.Sprintf("%d incidents detected", len(eps.Incidents))
+		fmt.Println(err)
+		return
 	}
 	for _, p := range eps.Incidents {
 		// ack all of the incidents
 		id := p.ID
+		// Don't ack pages.
+		for _, na := range noack {
+			if strings.Contains(p.Title, na) {
+				continue
+			}
+		}
 		client.ManageIncidentsWithContext(context.TODO(), email, []pagerduty.ManageIncidentsOptions{{ID: id, Status: "acknowledged"}})
+		fmt.Printf("%s acknowledged: %s", time.Now().Format(time.RFC3339), p.Title)
+		if !strings.HasSuffix(p.Title, "\n") {
+			fmt.Println()
+		}
+		if p.Body.Details != "" {
+			fmt.Printf("\t%s\n", p.Body.Details)
+		}
 	}
-
 }
 
 func main() {
-    c := cron.New()
-    c.AddFunc("@every 5s", checkAndAcknowledgeAlert)
-    c.Start()
-    done := make(chan os.Signal, 1)
-    signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
-    <-done
-    c.Stop()
+	if authToken == "" || userId == "" || email == "" {
+		fmt.Println("Missing env variables!!!")
+		os.Exit(1)
+	}
+
+	err := readnoack()
+	if err != nil {
+		fmt.Println("error reading noack file: ", err)
+	}
+
+	client = pagerduty.NewClient(authToken)
+
+	timer = time.AfterFunc(freq, checkAndAcknowledgeAlert)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+	timer.Stop()
+}
+
+func readnoack() error {
+	r, err := os.Open("noack")
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		t := scanner.Text()
+		if t == "" || strings.HasPrefix(t, "#") {
+			continue
+		}
+		noack = append(noack, t)
+	}
+	return nil
 }
